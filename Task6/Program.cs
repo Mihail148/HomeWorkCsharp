12b8d9f678ae6436d168c51163d504a5fa682c1a
Mihail Rkitcky <papi.pay@mail.ru>
2022-12-17 13:04:32 +0300

    Задача №6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №6 : Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 1)
+{
+    Console.WriteLine("Число " + number + " является: НЕЧЕТНЫМ");
+}
+else
+{
+    Console.WriteLine("Число " + number + " является: ЧЕТНЫМ");
+}
