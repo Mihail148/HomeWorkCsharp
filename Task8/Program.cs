5137fce097444f2b631032526aaa51c2719bbf1e
Mihail Rkitcky <papi.pay@mail.ru>
2022-12-17 13:11:19 +0300

    Задача №8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №8 : Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+int i = 1;
+bool not = true;
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Четные числа от 1 до " + number);
+while (i <= number)
+{
+    if (i % 2 !=1)
+    {
+        Console.Write(i + ", ");
+        not = false;
+    }
+    i++;
+}
+
+if (not)
+{
+    Console.WriteLine("Нет четных чисел!");
+}
