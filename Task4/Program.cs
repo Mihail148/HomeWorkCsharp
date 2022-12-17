53fbb329c8280c178f9c9389f654769a0d4c1b20
Mihail Rkitcky <papi.pay@mail.ru>
2022-12-17 12:57:48 +0300

    Задача №4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №4 : Напишите программу, которая принимает на вход три числа и выдает максимальное их этих чисел.
+
+Console.WriteLine("Введите 3 числа");
+int numberA = Convert.ToInt16(Console.ReadLine());
+int numberB = Convert.ToInt16(Console.ReadLine());
+int numberC = Convert.ToInt16(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+
+if (numberC > max)
+{
+    max = numberC;
+}
+
+Console.WriteLine("Наибольшее из введенных чисел -> " + max);
